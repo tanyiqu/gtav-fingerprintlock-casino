7410c188cfab18d6e56b0837a446a8cd3e1e68c1
Tanyiqu <1953649096@qq.com>
2020-10-19 11:28:52 +0800

    添加资源图片

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// 入口函数
+
+const fingerprints = [
+    'imgs/fingerprints/1.bmp',
+    'imgs/fingerprints/2.bmp',
+    'imgs/fingerprints/3.bmp',
+    'imgs/fingerprints/4.bmp'
+];
+
+const fingerprintsFragments = [
+    'imgs/fingerprints/1.1.bmp',
+    'imgs/fingerprints/1.2.bmp',
+    'imgs/fingerprints/1.3.bmp',
+    'imgs/fingerprints/1.4.bmp',
+];
+
+$(function () {
+
+
+    $('#img').attr('src', fingerprints[0]);
+});
